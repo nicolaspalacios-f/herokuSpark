4032ec7d8f2be32934dced7515c29e2ce4e3f483
nicolaspalacios-f <nicolas.palacios-f@mail.escuelaing.edu.co>
2022-08-12 12:26:58 -0500

    push inicial

---
+++ src/main/java/edu/escuelaing/arem/ASE/app/SparkWebApp.java
@@ -0,0 +1,17 @@
+package edu.escuelaing.arem.ASE.app;
+
+import static spark.Spark.*;
+
+public class SparkWebApp {
+    public static void main(String[] args) {
+        port(getPort());
+        get("/hello", (req, res) -> "Hello Heroku");
+    }
+
+    static int getPort() { // We get the port from the environment variable PORT
+        if (System.getenv("PORT") != null) {
+            return Integer.parseInt(System.getenv("PORT"));
+        }
+        return 8080;
+    }
+}
